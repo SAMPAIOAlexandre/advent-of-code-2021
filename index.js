f3efdec8c9a0fb689f93de5fefdf001ed515b633
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-04-03 00:13:47 +0200

    feat: part 1 function created

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ import { readFileSync } from 'fs';
 const test = readFileSync('test.txt', 'utf8');
 const final = readFileSync('final.txt', 'utf8');
 
-console.log({ test, final });
+const part1 = (data) => {
+  const lines = data.split(',');
+  return lines
+}
+
+console.log({'Part 1:': part1(test)});
+
+/* console.log({ test, final }); */
