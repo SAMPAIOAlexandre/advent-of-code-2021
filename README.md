8eafda3679bc5d90711af4f59d870f4eb1a09b5b
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-04-06 15:45:36 +0200

    docs: modification du readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# advent-of-code-2021
+# Advent-of-Code-2021
 
-https://adventofcode.com/2021/day/6
+Ce dépôt contient **ma solution au défi du jour 6** de [l'Advent of Code 2021](https://adventofcode.com/2021/day/6).
+
+## 📌 Objectifs personnels
+
+- Pratiquer la résolution de problèmes en JavaScript
+- Améliorer ma logique algorithmique
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import { readFileSync } from 'fs';
 const test = readFileSync('test.txt', 'utf8');
 const final = readFileSync('final.txt', 'utf8');
 
+// on va utiliser les données du test pour calculer le nombre de poissons qu'il y aura au bout de 80 jours
+// Chaque nombre représente un Poisson-lanterne (un petit poisson qui brille dans la nuit).
+//La valeur du nombre représente le nombre de jours qu'il reste avant qu'il ait un enfant (et donc crée un nouveau poisson-lanterne).
+//Ce nouveau poisson commencera sa vie avec la valeur de 8 jours avant d'avoir un bébé.
+
 const part1 = (data) => {
-  const lines = data.split(',');
+  const lines = data.split(' ');
   return lines
 }
 
